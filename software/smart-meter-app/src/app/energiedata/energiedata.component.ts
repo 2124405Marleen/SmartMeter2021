d7a3dee6bfadcd8de15d684837fdb0c68166f6cf
Marleen <mroubos2@avans.nl>
2021-03-27 14:20:50 +0100

    Cinvert data from string to the right type

--- software/smart-meter-app/src/app/energiedata/energiedata.component.ts
+++ software/smart-meter-app/src/app/energiedata/energiedata.component.ts
@@ -21,8 +21,6 @@ export interface Energiedata{
 export class EnergiedataComponent implements OnInit {
 
   energiedatas: Energiedata[] = [];
-  currectUsage: string;
-  currectGasUasge: string;
 
   constructor(http: HttpClient) {
     http.get('http://localhost:3300/energies').subscribe( (rec: Energiedata[]) => {
@@ -31,18 +29,15 @@ export class EnergiedataComponent implements OnInit {
       for (const output of rec) {
         tmp.push({
           timestamp: output.timestamp,
-          offPeakFlow: output.offPeakFlow.substring(1, 10),
-          peakFlow: output.peakFlow.substring(1, 10),
-          offPeakPowerFeed: output.offPeakPowerFeed.substring(1, 10),
-          peakPowerFeed: output.peakPowerFeed.substring(1, 10),
-          currentRate: output.currentRate,
-          currentPower: output.currentPower.substring(1, 6),
-          returnedPower: output.returnedPower.substring(1, 6),
-          gasUsage: output.gasUsage.substring(1, 9),
-        },
-          this.currectGasUasge = output.gasUsage.substring(1, 9),
-          this.currectUsage = output.currentPower.substring(1, 6),
-        );
+          offPeakFlow: parseFloat(output.offPeakFlow.substring(1, 10)),
+          peakFlow: parseFloat(output.peakFlow.substring(1, 10)),
+          offPeakPowerFeed: parseFloat(output.offPeakPowerFeed.substring(1, 10)),
+          peakPowerFeed: parseFloat(output.peakPowerFeed.substring(1, 10)),
+          currentRate: Number(output.currentRate),
+          currentPower: parseFloat(output.currentPower.substring(1, 6)),
+          returnedPower: parseFloat(output.returnedPower.substring(1, 6)),
+          gasUsage: parseFloat(output.gasUsage.substring(1, 9)),
+        });
         this.energiedatas = tmp;
       }
     });
